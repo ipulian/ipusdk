baeb16f550af83a61f67029e9d3c4538f80e2c60
ipulian <team.github@51lianlian.com>
2021-05-25 10:41:09 +0800

    修复bug

---
+++ context/src/main/java/com/ipusoft/context/base/IObserver.java
@@ -0,0 +1,33 @@
+package com.ipusoft.context.base;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+/**
+ * author : GWFan
+ * time   : 5/24/21 6:51 PM
+ * desc   :
+ */
+
+public class IObserver<T> implements Observer<T> {
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+
+    }
+
+    @Override
+    public void onNext(@NonNull T t) {
+
+    }
+
+    @Override
+    public void onError(@NonNull Throwable e) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+}
