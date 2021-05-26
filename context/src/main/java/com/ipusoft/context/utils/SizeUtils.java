b0bb616a1daaff115ec62baaf7396b4ebbad590e
ipulian <team.github@51lianlian.com>
2021-05-26 16:36:39 +0800

    优化

---
+++ context/src/main/java/com/ipusoft/context/utils/SizeUtils.java
@@ -0,0 +1,31 @@
+package com.ipusoft.context.utils;
+/**
+ * author : GWFan
+ * time   : 5/2/21 9:50 PM
+ * desc   :
+ */
+
+import android.content.res.Resources;
+
+public class SizeUtils {
+
+    public static int dp2px(final float dpValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dp(final float pxValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int sp2px(final float spValue) {
+        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int px2sp(final float pxValue) {
+        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+}
