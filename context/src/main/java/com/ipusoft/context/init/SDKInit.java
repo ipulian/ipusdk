92256b23d81a6196d0bde0cb980f45305fa66612
ipulian <team.github@51lianlian.com>
2021-05-27 17:51:14 +0800

    bug

--- context/src/main/java/com/ipusoft/context/init/SDKInit.java
+++ context/src/main/java/com/ipusoft/context/init/SDKInit.java
@@ -3,9 +3,9 @@ package com.ipusoft.context.init;
 import com.ipusoft.context.OnSDKLoginListener;
 import com.ipusoft.context.bean.IAuthInfo;
 import com.ipusoft.context.http.AuthHttp;
+import com.ipusoft.context.utils.MD5Utils;
 import com.ipusoft.context.utils.StringUtils;
 
-import java.security.MessageDigest;
 import java.util.Date;
 
 /**
@@ -51,7 +51,7 @@ public class SDKInit {
 
     private static String getSign(String key, String secret, String username) {
         System.currentTimeMillis();
-        return getMD5("dev=SDK&key=" + key + "&ts=" + getSecondTimestamp(new Date())
+        return MD5Utils.getMD5("dev=SDK&key=" + key + "&ts=" + getSecondTimestamp(new Date())
                 + "&username=" + username + secret);
     }
 
@@ -69,28 +69,5 @@ public class SDKInit {
         return Integer.parseInt(timestamp);
     }
 
-    private static String getMD5(String inStr) {
-        MessageDigest md5;
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            e.printStackTrace();
-            return "";
-        }
-        char[] charArray = inStr.toCharArray();
-        byte[] byteArray = new byte[charArray.length];
-        for (int i = 0; i < charArray.length; i++)
-            byteArray[i] = (byte) charArray[i];
 
-        byte[] md5Bytes = md5.digest(byteArray);
-        StringBuilder hexValue = new StringBuilder();
-        for (byte md5Byte : md5Bytes) {
-            int val = ((int) md5Byte) & 0xff;
-            if (val < 16)
-                hexValue.append("0");
-            hexValue.append(Integer.toHexString(val));
-        }
-        return hexValue.toString();
-    }
 }
